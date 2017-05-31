1fa85ac7d3b136fe3affee05dd77be53bd2f5774
Xindong Xu <xindong.xu@amlogic.com>
2017-05-31 14:18:28 +0800

    PD#145255: generate amlogic files to vendor/
    Change-Id: Ib37d1296441de6e7af720f5de6075a4da3ed0cf2

--- Android.mk
+++ Android.mk
@@ -9,6 +9,10 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := PPPoE
 LOCAL_CERTIFICATE := platform
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
